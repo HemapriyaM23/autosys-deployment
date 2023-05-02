f3c90e1ec93fa9ca863e0f1b437f9e7ad835fb5c
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-05-02 14:06:54 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+ #!/bin/ksh
+
+
+
+gitclonebasepath=`pwd`
+
+
+#Defining the work folder
+workfolder="${gitclonebasepath}/../../../deployfolder"
+echo "${workfolder}"
+scpfolder="${workfolder}/autosys_scp"
+autosyslistfile="${scpfolder}/autosysfile.jil"
+echo "${autosyslistfile}"
+deploystatus=0
+
+
+#Paths for list and Autosys Base path
+listfile="${gitclonebasepath}/ListFiles/autosys_param.lst"
+echo "ls ${listfile}"
+dos2unix ${listfile}
+autosysbasepath="${gitclonebasepath}/Autosys"
+
+`touch ${autosyslistfile}`
+echo "autosyslistfile created"
