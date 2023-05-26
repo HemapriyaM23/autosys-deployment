5b25de4886a2722f4d4eb23930e37f134467dd4e
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-05-26 12:58:39 +0530

    Update deploy.sh

--- DevOps_Scripts/deploy.sh
+++ DevOps_Scripts/deploy.sh
@@ -15,10 +15,42 @@ jilFiles=$(find ${jilDirectory} -name '*.jil')
 # Iterate over the JIL files and make POST requests
 for jilFile in ${jilFiles}; do
     echo "Processing file: ${jilFile}"
-    #if [ "${target_branch}" = "dev" ]; then
+    if [ "${target_branch}" = "test" ]; then
         # Replace string in the JIL file for the "test" branch
-        #sed -i 's/insert/update/' "${jilFile}"
-    #fi
+        sed -i 's/d2compaus/t2compaus/g' "${jilFile}"
+        sed -i 's/D2COMPAUS/T2COMPAUS/g' "${jilFile}"
+        sed -i 's/amrvopsfa000001/amrvotpa000001/g' "${jilFile}"
+        sed -i 's/pa_matillion_master.ksh PALIGN DEV PFZALGN_DEV PFZALGN_DEV/pa_matillion_master.ksh PALIGN TEST PFZALGN_TEST PFZALGN_TEST/g' "${jilFile}"
+        sed -i 's/pa_postgresql_master.ksh PFZALGN DEV/pa_postgresql_master.ksh PFZALGN TEST/g' "${jilFile}"
+        sed -i 's/pa_snowflake_master.ksh cometl_pa_ods_dev/pa_snowflake_master.ksh cometl_pa_ods_test/g' "${jilFile}"
+    fi
+    if [ "${target_branch}" = "sit" ]; then
+        # Replace string in the JIL file for the "test" branch
+        sed -i 's/t2compaus/s2compaus/g' "${jilFile}"
+        sed -i 's/T2COMPAUS/S2COMPAUS/g' "${jilFile}"
+        sed -i 's/amrvotpa000001/amrvoupa000001/g' "${jilFile}"
+        sed -i 's/pa_matillion_master.ksh PALIGN TEST PFZALGN_TEST PFZALGN_TEST/pa_matillion_master.ksh PALIGN SIT PFZALGN PFZALGN/g' "${jilFile}"
+        sed -i 's/pa_postgresql_master.ksh PFZALGN TEST/pa_postgresql_master.ksh PFZALGN SIT/g' "${jilFile}"
+        sed -i 's/pa_snowflake_master.ksh cometl_pa_ods_test/pa_snowflake_master.ksh cometl_pa_ods/g' "${jilFile}"
+    fi
+    if [ "${target_branch}" = "uat" ]; then
+        # Replace string in the JIL file for the "test" branch
+        sed -i 's/s2compaus/u2compaus/g' "${jilFile}"
+        sed -i 's/S2COMPAUS/U2COMPAUS/g' "${jilFile}"
+        sed -i 's/amrvoupa000001/amrvospa000002/g' "${jilFile}"
+        sed -i 's/a_matillion_master.ksh PALIGN SIT PFZALGN PFZALGN/pa_matillion_master.ksh PALIGN UAT PFZALGN_STG PFZALGN_STG/g' "${jilFile}"
+        sed -i 's/pa_postgresql_master.ksh PFZALGN SIT/pa_postgresql_master.ksh PFZALGN UAT/g' "${jilFile}"
+        sed -i 's/pa_snowflake_master.ksh cometl_pa_ods/pa_snowflake_master.ksh cometl_pa_stg/g' "${jilFile}"
+    fi
+    if [ "${target_branch}" = "prod" ]; then
+        # Replace string in the JIL file for the "test" branch
+        sed -i 's/u2compaus/p2compaus/g' "${jilFile}"
+        sed -i 's/U2COMPAUS/P2COMPAUS/g' "${jilFile}"
+        #sed -i 's/amrvoupa000001/amrvospa000002/g' "${jilFile}"
+        sed -i 's/pa_matillion_master.ksh PALIGN UAT PFZALGN_STG PFZALGN_STG/pa_matillion_master.ksh PALIGN PROD PFZALGN PFZALGN/g' "${jilFile}"
+        sed -i 's/pa_postgresql_master.ksh PFZALGN UAT/pa_postgresql_master.ksh PFZALGN PROD/g' "${jilFile}"
+        sed -i 's/pa_snowflake_master.ksh cometl_pa_stg/pa_snowflake_master.ksh cometl_pa_prod/g' "${jilFile}"
+    fi
     # Perform the curl command
     response=$(curl -X POST -H 'Content-Type: text/plain' --upload-file "${jilFile}" ${apiEndpoint} -k --user "${USERNAME}:${PASSWORD}" -i)
     
